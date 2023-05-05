35a497db83dfacc549feab3eaf349720f9936982
Glitch (location-weather-api) <none>
2023-05-05 14:13:13 +0000

    stefanbohacek/location-weather-api

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express'),
-      bodyParser = require('body-parser'),
-      app = express();
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  app = express();
 
 /* Set up the app. */
 
-app.use( express.static( 'public' ) );
-app.use( bodyParser.json() );
-app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Set up the routes. */
 
-app.use('/weather', require('./routes/weather.js'));
-app.use('/background', require('./routes/background.js'));
+app.use("/weather", require("./routes/weather.js"));
+app.use("/background", require("./routes/background.js"));
 
-app.get('/', function( req, res ) {
-  res.setHeader('Content-Type', 'application/json');
-  res.end( JSON.stringify( {
-    error: 'nothing here'
-  } ) );
+app.get("/", function (req, res) {
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      error: "nothing here",
+    })
+  );
 });
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log( 'Your app is listening on port ' + listener.address().port );
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
